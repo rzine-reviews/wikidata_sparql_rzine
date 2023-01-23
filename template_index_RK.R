2f39b7e68a1dbdd224dee128e6b47d4a04dc8d89
Madouck <raphaelle.krummeich@univ-rouen.fr>
2023-01-23 18:29:17 +0100

    màj/nettoyage repos

--- template_index_RK.R
+++
@@ -1,5 +0,0 @@
-rmarkdown::draft(file = "index_RK.Rmd", 
-                 template = "readrzine", 
-                 package = "rzine", 
-                 create_dir = FALSE, 
-                 edit = FALSE)
